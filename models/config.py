455e325f90f05c6c17b888e0d7dba8df8fce617b
ankitnandwani <ankit.nandwani@dpworld.com>
2026-07-20 04:31:39 +0530

    fix

--- models/config.py
+++ models/config.py
@@ -3,7 +3,3 @@ import os
 BASE_URL = "https://api.upstox.com/v2"
 BASE_URL_V3 = "https://api.upstox.com/v3"
 UPSTOX_ACCESS_TOKEN = os.getenv("UPSTOX_ACCESS_TOKEN")
-if not UPSTOX_ACCESS_TOKEN:
-    raise RuntimeError(
-        "UPSTOX_ACCESS_TOKEN is not set. Please add it to your ~/.zshrc."
-    )
